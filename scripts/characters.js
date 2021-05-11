78a7daae516e286d2a66da634804281738b85181
UmPdroo <pedrohenrisantosfontoura@gmail.com>
2021-05-10 22:36:46 -0300

    sistema de busca aprimorado

--- scripts/characters.js
+++ scripts/characters.js
@@ -1,26 +1,26 @@
 const Form = {
-	searchTearm: document.querySelector("input#search-bar").value,
+	searchTearm: "",
 
 	submit(event) {
+		Form.searchTearm = document.querySelector("input#search-bar").value;
 		event.preventDefault();
 		try {
-			let url = `https://swapi.dev/api/people/?search=${Form.searchTearm}`;
+			fetch(
+				`https://swapi.dev/api/people/?search=${Form.searchTearm}`
+			).then((response) => {
+				response.json().then((data) => {
+					searchResults = data.results;
 
-			let xhttp = new XMLHttpRequest();
-			xhttp.open("GET", url, true);
-
-			xhttp.onreadystatechange = function () {
-				if (xhttp.readyState == 4 && xhttp.status == 200) {
-					let results = JSON.parse(xhttp.responseText);
-					searchResults = results.results;
-					DOM.updateResultList();
-					console.log(url);
-				}
-			};
-
-			xhttp.send();
-			Form.searchTearm = "";
-			Form.searchTearm.value = "";
+					for (let i = 0; i < searchResults.length; i++) {
+						fetch(searchResults[i].homeworld).then((response) => {
+							response.json().then((data) => {
+								searchResults[i].homeworld = data.name;
+								DOM.updateResultList();
+							});
+						});
+					}
+				});
+			});
 		} catch (error) {
 			alert(error.message);
 		}
@@ -32,9 +32,15 @@ let searchResults = [];
 const DOM = {
 	searchResultsContainer: document.querySelector(".search-results"),
 
-	addResult(result) {
+	addResult(result, inner) {
 		const li = document.createElement("li");
 		li.innerHTML = DOM.innerHTMLsearchResult(result);
+
+		DOM.searchResultsContainer.appendChild(li);
+	},
+	addKnowMore(result, inner) {
+		const li = document.createElement("li");
+		li.innerHTML = DOM.innerHTMLknowMore(result);
 
 		DOM.searchResultsContainer.appendChild(li);
 	},
@@ -42,23 +48,54 @@ const DOM = {
 	innerHTMLsearchResult(result) {
 		let gender = Utils.translateGender(result.gender);
 
-		// let homeworld = Utils.formatHomeworld(result.homeworld);
-
 		const html = `
 		<span id="name">${result.name}</span>
 		<span id="birth-year">Nascimento: <strong>${result.birth_year}</strong></span>
 		<span id="gender">Genero: <strong>${gender}</strong></span>
+		<span id="homeworld">Planeta Natal: <strong>${result.homeworld}</strong></span>
 		`;
-		// <span id="homeworld">Planeta Natal: <strong>${result.homeworld}</strong></span>
+
+		return html;
+	},
+	innerHTMLknowMore(result) {
+		let gender = Utils.translateGender(result.gender);
+		let birth_year = Utils.formatBirth(result.birth_year);
+		let homeworld = Utils.formatHomeworld(result.homeworld);
+
+		const html = `
+		<span id="name">${result.name}</span>
+		<span id="birth-year">Nascimento: <strong>${birth_year}</strong></span>
+		<span id="gender">Genero: <strong>${gender}</strong></span>
+		<span id="homeworld">Planeta Natal: <strong>${homeworld}</strong></span>
+		`;
 
 		return html;
 	},
 
 	updateResultList() {
+
+		Form.searchTearm.value = "";
+		Form.searchTearm = "";
 		DOM.searchResultsContainer.innerHTML = "";
 
 		for (let i = 0; i < searchResults.length; i++) {
 			DOM.addResult(searchResults[i]);
+		}
+	},
+	updateKnowMoreList() {
+		console.log(searchResults);
+
+		for (let i = 0; i < searchResults.length; i++) {
+			fetch(searchResults[i].homeworld).then((response) => {
+				response.json().then((data) => {
+					searchResults[i].homeworld = data.name;
+				});
+			});
+		}
+		DOM.searchResultsContainer.innerHTML = "";
+
+		for (let i = 0; i < searchResults.length; i++) {
+			DOM.addKnowMore(searchResults[i]);
 		}
 	},
 };
@@ -71,23 +108,53 @@ const Utils = {
 			return "Feminino";
 		}
 	},
-
-	/*formatHomeworld(homeworld) {
-		let url = homeworld;
-		let homeworldName;
-
-		let xhttp = new XMLHttpRequest();
-		xhttp.open("GET", url, true);
-
-		xhttp.onreadystatechange = function () {
-			if (xhttp.readyState == 4 && xhttp.status == 200) {
-				let result = JSON.parse(xhttp.responseText);
-				let actualHomeworld = result.name;
-				console.log(actualHomeworld);
-				return actualHomeworld;
-			}
-		};
-
-		xhttp.send();
-	},*/
+	formatBirth(birth) {
+		if (birth == "unknown") {
+			return "N/A";
+		} else {
+			return birth;
+		}
+	},
+	formatHomeworld(homeworld) {
+		if (homeworld.includes("http://swapi.dev/api/") == true) {
+			return "Não especificado";
+		} else {
+			return homeworld;
+		}
+	},
+	limitSearchResults() {
+		while (searchResults.length > 6) {
+			searchResults.pop();
+		}
+	},
 };
+
+const App = {
+	init() {
+		App.knowMore();
+	},
+	knowMore() {
+		searchResults = [];
+		let randomSearch = [];
+		for (let i = 0; i < 3; i++) {
+			randomSearch.push(Math.floor(Math.random() * (82 - 1) + 1));
+		}
+
+		for (let i = 0; i < randomSearch.length; i++) {
+			fetch(`https://swapi.dev/api/people/${randomSearch[i]}`).then(
+				(response) => {
+					response.json().then((data) => {
+						searchResults.push(data);
+					});
+				}
+			);
+		}
+
+		setTimeout(() => {
+			DOM.updateKnowMoreList();
+		}, 1000);
+		console.log(randomSearch);
+	},
+};
+
+App.init();
--- scripts/index.js
+++ scripts/index.js
@@ -1,110 +1,52 @@
-let url = "https://swapi.dev/api/films/";
-
-let xhttp = new XMLHttpRequest();
-xhttp.open("GET", url, true);
-
-xhttp.onreadystatechange = function () {
-    if (xhttp.readyState == 4 && xhttp.status == 200) {
-        Movies = JSON.parse(xhttp.responseText);
-        App.init();
-    }
-};
-
-xhttp.send();
+fetch(`https://swapi.dev/api/films/`).then((response) => {
+	response.json().then((data) => {
+		(Movies = data.results), App.init();
+	});
+});
 
 let Movies = [];
 
 const DOM = {
-    movieTableContainer: document.querySelector(".movieList"),
-    addMovie(movie, i) {
-        const tr = document.createElement("tr");
-        tr.innerHTML = DOM.innerHTMLMovie(movie, i);
+	movieTableContainer: document.querySelector(".movieList"),
+	addMovie(movie, i) {
+		const tr = document.createElement("tr");
+		tr.innerHTML = DOM.innerHTMLMovie(movie, i);
 
-        DOM.movieTableContainer.appendChild(tr);
-    },
-    innerHTMLMovie(movie, i) {
-        let title = Utils.formatTitle(i);
-        let release_date = Utils.formatYear(i);
+		DOM.movieTableContainer.appendChild(tr);
+	},
+	innerHTMLMovie(movie, i) {
+		let title = Utils.formatTitle(i);
+		let release_date = Utils.formatYear(i);
 
-        const html = `
+		const html = `
         <td>${title}</td>
         <td>${release_date}</td>
         <td>${movie.director}</td>
         `;
 
-        return html;
-    },
+		return html;
+	},
 };
 
 const Utils = {
-    formatTitle(i) {
-        let title = Movies.results[i].title;
-        let ep = Movies.results[i].episode_id;
+	formatTitle(i) {
+		let title = Movies[i].title;
+		let ep = Movies[i].episode_id;
 
-        return `Episodio ${ep} - ${title}`;
-    },
-    formatYear(i) {
-        let date = Movies.results[i].release_date;
-        let splittedYear = date.split("-");
+		return `Episodio ${ep} - ${title}`;
+	},
+	formatYear(i) {
+		let date = Movies[i].release_date;
+		let splittedYear = date.split("-");
 
-        return splittedYear[0];
-    },
+		return splittedYear[0];
+	},
 };
 
 const App = {
-    verifyInitialization() {
-        while (Movies.results == undefined) {
-            App.verifyInitialization();
-        }
-    },
-    init() {
-        for (let i = 0; i < 6; i++) {
-            DOM.addMovie(Movies.results[i], i);
-        }
-    },
+	init() {
+		for (let i = 0; i < Movies.length; i++) {
+			DOM.addMovie(Movies[i], i);
+		}
+	},
 };
-
-
-
-function levenshteinDistance(source, target) {
-    var result = []; // This is going to be a matrix. The result we're looking for can be found by flood-filling it.
-    var i, j;
-
-    // Let's fill the first line of the matrix with distances from each substring of the source to the empty string.
-    for (i = 0; i <= source.length; i++) {
-        result.push([i]);
-    }
-
-    // Same as above, but for a column. Also, the first element of the first column is already filled with a zero, hence j = 1.
-    for (j = 1; j <= target.length; j++) {
-        result[0].push(j);
-    }
-
-    for (i = 1; i <= source.length; i++) {
-        for (j = 1; j <= target.length; j++) {
-            // the element in the iteration doesn't exist yet. Let's create it.
-            result[i].push(0);
-
-            // Now let's get an edit distance between two characters.
-            if (source[i - 1] == target[j - 1]) {
-                // Notice how, for a comparison between two equal strings, the diagonal will be filled with zeroes.
-                result[i][j] = result[i - 1][j - 1];
-            } else {
-                // Different characters. The formulae for edit distances are shown below.
-                var minimum = Math.min(
-                    result[i - 1][j] + 1, // This means we can bring the target string closer to the source one with a character deletion
-                    result[i][j - 1] + 1 // Same as above, but with a character insertion
-                );
-
-                minimum = Math.min(
-                    minimum,
-                    result[i - 1][j - 1] + 1 // This means that we can bring the target closer to the source by means of a character change.
-                );
-                result[i][j] = minimum;
-            }
-        }
-    }
-
-    // The actual distance we're looking for is the value stored in the lower right corner of the matrix.
-    return result[source.length][target.length];
-}
