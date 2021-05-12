38e208e7633b2557fd0b0e0ebe1f637f7777f457
UmPdroo <pedrohenrisantosfontoura@gmail.com>
2021-05-12 00:23:53 -0300

    pesquisa aprimorada

--- scripts/search.js
+++ scripts/search.js
@@ -1,23 +1,32 @@
+let loading = document.querySelector(".loading");
 const Form = {
 	searchTearm: "",
+	searchType: "",
 
 	submit(event) {
-		Form.searchTearm = document.querySelector("input#search-bar").value;
+		this.searchTearm = document.querySelector("input#search-bar").value;
 		event.preventDefault();
 		try {
+			loading.classList.add("on");
 			fetch(
-				`https://swapi.dev/api/people/?search=${Form.searchTearm}`
+				`https://swapi.dev/api/${this.searchType}/?search=${this.searchTearm}`
 			).then((response) => {
 				response.json().then((data) => {
 					searchResults = data.results;
 
-					for (let i = 0; i < searchResults.length; i++) {
-						fetch(searchResults[i].homeworld).then((response) => {
-							response.json().then((data) => {
-								searchResults[i].homeworld = data.name;
-								DOM.updateResultList();
-							});
-						});
+					if (this.searchType === "people") {
+						for (let i = 0; i < searchResults.length; i++) {
+							fetch(searchResults[i].homeworld).then(
+								(response) => {
+									response.json().then((data) => {
+										searchResults[i].homeworld = data.name;
+										DOM.updateResultList(this.searchType);
+									});
+								}
+							);
+						}
+					} else {
+						DOM.updateResultList(this.searchType);
 					}
 				});
 			});
@@ -32,30 +41,43 @@ let searchResults = [];
 const DOM = {
 	searchResultsContainer: document.querySelector(".search-results"),
 
-	addResult(result, inner) {
+	addResult(result, type) {
 		const li = document.createElement("li");
-		li.innerHTML = DOM.innerHTMLsearchResult(result);
+		li.innerHTML = DOM.innerHTMLsearchResult(result, type);
 
 		DOM.searchResultsContainer.appendChild(li);
 	},
-	addKnowMore(result, inner) {
+	addKnowMore(result) {
 		const li = document.createElement("li");
 		li.innerHTML = DOM.innerHTMLknowMore(result);
 
 		DOM.searchResultsContainer.appendChild(li);
 	},
 
-	innerHTMLsearchResult(result) {
-		let gender = Utils.translateGender(result.gender);
+	innerHTMLsearchResult(result, type) {
+		if (type === "people") {
+			let gender = Utils.translateGender(result.gender);
 
-		const html = `
-		<span id="name">${result.name}</span>
-		<span id="birth-year">Nascimento: <strong>${result.birth_year}</strong></span>
-		<span id="gender">Genero: <strong>${gender}</strong></span>
-		<span id="homeworld">Planeta Natal: <strong>${result.homeworld}</strong></span>
-		`;
+			const html = `
+			<span id="name">${result.name}</span>
+			<span id="birth-year">Nascimento: <strong>${result.birth_year}</strong></span>
+			<span id="gender">Genero: <strong>${gender}</strong></span>
+			<span id="homeworld">Planeta Natal: <strong>${result.homeworld}</strong></span>
+			`;
 
-		return html;
+			return html;
+		} else if (type === "starships") {
+			let gender = Utils.translateGender(result.gender);
+
+			const html = `
+			<span id="name">${result.name}</span>
+			<span id="model">Modelo: <strong>${result.model}</strong></span>
+			<span id="manufactory">Fabricante: <strong>${result.manufacturer}</strong></span>
+			<span id="credits">Custo em creditos: <strong>${result.cost_in_credits}</strong></span>
+			`;
+
+			return html;
+		}
 	},
 	innerHTMLknowMore(result) {
 		let gender = Utils.translateGender(result.gender);
@@ -72,26 +94,20 @@ const DOM = {
 		return html;
 	},
 
-	updateResultList() {
-
+	updateResultList(type) {
+		loading.classList.remove("on");
 		Form.searchTearm.value = "";
 		Form.searchTearm = "";
 		DOM.searchResultsContainer.innerHTML = "";
 
 		for (let i = 0; i < searchResults.length; i++) {
-			DOM.addResult(searchResults[i]);
+			DOM.addResult(searchResults[i], type);
 		}
 	},
 	updateKnowMoreList() {
+		loading.classList.remove("on");
 		console.log(searchResults);
 
-		for (let i = 0; i < searchResults.length; i++) {
-			fetch(searchResults[i].homeworld).then((response) => {
-				response.json().then((data) => {
-					searchResults[i].homeworld = data.name;
-				});
-			});
-		}
 		DOM.searchResultsContainer.innerHTML = "";
 
 		for (let i = 0; i < searchResults.length; i++) {
@@ -127,11 +143,37 @@ const Utils = {
 			searchResults.pop();
 		}
 	},
+	setSearchType(type) {
+		Form.searchType = type;
+		switch (type) {
+			case "people":
+				document.querySelector(
+					"input#search-bar"
+				).placeholder = `Pesquisando por personagens`;
+				break;
+
+			case "starships":
+				document.querySelector(
+					"input#search-bar"
+				).placeholder = `Pesquisando por naves`;
+				break;
+
+			case "planets":
+				document.querySelector(
+					"input#search-bar"
+				).placeholder = `Pesquisando por planetas e luas`;
+				break;
+
+			default:
+				break;
+		}
+	},
 };
 
 const App = {
 	init() {
 		App.knowMore();
+		Utils.setSearchType("people");
 	},
 	knowMore() {
 		searchResults = [];
@@ -152,7 +194,7 @@ const App = {
 
 		setTimeout(() => {
 			DOM.updateKnowMoreList();
-		}, 1000);
+		}, 2000);
 		console.log(randomSearch);
 	},
 };
